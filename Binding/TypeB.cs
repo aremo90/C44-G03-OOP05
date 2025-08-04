591b6f4f08c3c371f5979175693cfee1e0de6914
aremo90 <mrhamza2005@gmail.com>
2025-08-04 08:37:22 +0300

    02 Binding

---
+++ Binding/TypeB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Binding
+{
+    internal class TypeB : TypeA
+    {
+        public int B { get; set; }
+
+        public TypeB(int a, int b) : base(a)
+        {
+            B = b;
+        }
+
+        // Apply method overriding 'new' in function 01
+        public new void Func01()
+        {
+            Console.WriteLine("Child Class");
+        }
+
+        // Apply method overriding 'override' in function 02
+        //Method itself
+        //      must be public
+        //      must be virtual    
+        public override void Func02()
+        {
+            Console.WriteLine($"TypeB: A = {A}, B = {B}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.Binding;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Binding 
+            /*
+             * Refrerence From Parent refer to object from Child
+             */
+
+            //TypeA type = new TypeB(1,2);
+
+            //type.A = 1;
+            ////type.B = 2;
+
+            //type.Func01(); // Output: Parent Class
+
+            //type.Func02(); // Go to last Override of function
+            #endregion 
+
         }
     }
 }
